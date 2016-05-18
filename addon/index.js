4816d8a0fbe3a238130bcc852860eecc46221a5e
frank06 <me@frank06.net>
2016-05-19 02:04:10 +0600

    support belongsto, add more tests

--- addon/index.js
+++ addon/index.js
@@ -2,44 +2,62 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   
-  serializeHasMany(snapshot, data, relationship) {
+  serializeRelationship(snapshot, data, rel) {
     
-    this._super(snapshot, data, relationship);
+    const kind = rel.kind;
     
     snapshot.eachRelationship(relationship => {
       
       if (this.get(`attrs.${relationship}.serialize`) === true) {
-        
-        const array = snapshot.record.get(relationship);
 
         data.relationships = data.relationships || {};
         data.relationships[relationship] = data.relationships[relationship] || {};
-        data.relationships[relationship].data = array.map(obj => {
         
-          const serialized = obj.serialize();
-
-          if (obj.id) {
-            serialized.data.id = obj.id;
-          } else {
-            serialized.data.attributes.__id__ = obj.get('_internalModel')[Ember.GUID_KEY];
-          }
-          
-          // do not allow embedded relationships
-          delete serialized.data.relationships;
+        if (kind === "belongsTo") {
+          data.relationships[relationship].data = this.serializeRecord(snapshot.belongsTo(relationship));
+        }
         
-          return serialized.data;
-        
-        });
+        if (kind === "hasMany") {
+          data.relationships[relationship].data = snapshot.hasMany(relationship).map(this.serializeRecord);
+        }
         
       }
 
     });
 
   },
+  
+  serializeRecord(obj) {
+    
+    const serialized = obj.serialize();
+
+    if (obj.id) {
+      serialized.data.id = obj.id;
+    } else {
+      serialized.data.attributes.__id__ = obj.record.get('_internalModel')[Ember.GUID_KEY];
+    }
+    
+    // do not allow embedded relationships
+    delete serialized.data.relationships;
+  
+    return serialized.data;
+  
+  },
+  
+  serializeHasMany() {
+    this._super(...arguments);
+    this.serializeRelationship(...arguments);
+  },
+  
+  serializeBelongsTo() {
+    this._super(...arguments);
+    this.serializeRelationship(...arguments);
+  },
 
   normalizeSaveResponse(store, modelName, obj) {
     
     const rels = obj.data.relationships || [];
+    
     Object.keys(rels).forEach(rel => {
       
       rels[rel].data.forEach(elem => {
--- tests/integration/serializers/save-relationships-mixin-test.js
+++ tests/integration/serializers/save-relationships-mixin-test.js
@@ -37,9 +37,6 @@ module('serializers/save-relationships-mixin', {
         
     store = container.lookup('service:store');
     
-    // store.modelFor('artist');
-    // store.modelFor('album');
-    
     registry.register('adapter:application', DS.JSONAPIAdapter);
     registry.register('serializer:application', DS.JSONAPISerializer);
     
@@ -160,5 +157,79 @@ test("serialize artist with embedded albums (with and without ID)", function(ass
   
   // __id__ should be present in album without id
   assert.ok(artistJSON.data.relationships.albums.data[1].attributes.__id__);
+
+});
+
+test("serialize album with embedded belongs-to artist (without ID)", function(assert) {
+  
+  registry.register('serializer:album', DS.JSONAPISerializer.extend(SaveRelationshipsMixin, {
+    attrs: {
+      artist: { serialize: true }
+    }
+  }));
+  
+  registry.register('serializer:artist', DS.JSONAPISerializer.extend(SaveRelationshipsMixin));
+
+  const serializer = store.serializerFor("album");
+  let albumJSON;
+  
+  Ember.run(function() {
+    
+    const album = store.createRecord('album', { name: "Kid A" });
+    const artist = store.createRecord('artist', { name: "Radiohead" });
+    album.set('artist', artist);
+  
+    albumJSON = serializer.serialize(album._createSnapshot());
+
+  });
+  
+  assert.equal(albumJSON.data.relationships.artist.data.attributes.name, "Radiohead");
+
+});
+
+test("normalize artist + album", function(assert) {
+  
+  registry.register('serializer:artist', DS.JSONAPISerializer.extend(SaveRelationshipsMixin, {
+    attrs: {
+      albums: { serialize: true }
+    }
+  }));
+  
+  registry.register('serializer:album', DS.JSONAPISerializer.extend(SaveRelationshipsMixin));
+
+  const serializer = store.serializerFor("artist");
+  let artistJSON;
+  
+  Ember.run(function() {
+    
+    const artist = store.createRecord('artist', { name: "Radiohead" });
+    const album1 = store.createRecord('album', { name: "Kid A" });
+    const album2 = store.createRecord('album', { id: "2", name: "Kid B" });
+    artist.get('albums').pushObjects([album1, album2]);
+  
+    artistJSON = serializer.serialize(artist._createSnapshot());
+    
+    const internalId = artistJSON.data.relationships.albums.data[0].attributes.__id__;
+    
+    const serverJSON = { data: 
+     { id: "1",
+       attributes: { name: 'Radiohead' },
+       relationships: { albums: { data: 
+     [ { id: "89329", attributes: { name: "Kid A", __id__: internalId }, type: 'album' },
+       { id: "2", attributes: { name: "Kid B2" }, type: 'album' } ] } },
+       type: 'artist' } };
+    
+    serializer.normalizeResponse(store, Artist, serverJSON, '1', 'createRecord');
+
+  });
+  
+  // first album should be in a saved state and have an id
+  const firstAlbum = store.peekAll('album').findBy("name", "Kid A");
+  assert.equal(firstAlbum.get('currentState.stateName'), "root.loaded.saved");
+  assert.equal(firstAlbum.get('id'), "89329");
+  
+  // second album should have updated name -- TODO check should be B2???
+  const secondAlbum = store.peekAll('album').objectAt(0);
+  assert.equal(secondAlbum.get('name'), "Kid B");
 
 });
